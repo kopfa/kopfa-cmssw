9511f02a32b49837579a0663c3e45e24c746ac13
Taejeong <monttj@gmail.com>
2013-11-07 21:39:30 +0100

    modified for lepton filter

--- CMGTools/Common/python/skims/cmgElectronSel_cfi.py
+++ CMGTools/Common/python/skims/cmgElectronSel_cfi.py
@@ -4,10 +4,21 @@ import FWCore.ParameterSet.Config as cms
 # as they are subject to change. 
 # you should override these cuts in your analysis.
 
-cmgElectronSel = cms.EDFilter(
-    "CmgElectronSelector",
-    src = cms.InputTag("cmgElectron"),
-    cut = cms.string( "pt()>15" )
-    )
+#cmgElectronSel = cms.EDFilter(
+#    "CmgElectronSelector",
+#    src = cms.InputTag("cmgElectron"),
+#    cut = cms.string( "pt()>20 " )
+#    )
 
+cmgElectronSel = cms.EDFilter('CMGElectronProducer',
+    applyFilter = cms.untracked.bool(False),
+    electronLabel = cms.InputTag("cmgElectron"),
+    vertexLabel = cms.untracked.InputTag('offlinePrimaryVertices'),
+    rhoIsoLabel =  cms.untracked.InputTag('kt6PFJets','rho'),
+    ptcut = cms.untracked.double(20),
+    etacut = cms.untracked.double(2.4),
+    mvacut = cms.untracked.double(0.5),
+    relIso = cms.untracked.double(0.15),
+    numberOfHits = cms.untracked.uint32(0),
+)
 
